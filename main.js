9a34b31e7c8d9d932b105f54c42b9ecc41285573
Karlperlas <karlperlas.o2jammer@gmail.com>
2023-09-22 12:26:03 +0000

    www

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function sigePindot() {
-    alert("bat mo pinindot? Pumayag ba ko?");
-}
+// let sidebar = document.querySelector(".sidebar");
+// let isWide = false;
+
+// function widen() {
+//     if(isWide == false) {
+//         sidebar.style.width = "250px";
+//         isWide = true;
+//     } else if (isWide == true) {
+//         sidebar.style.width = "70px";
+//         isWide = false;
+//     }
+// }
+// let pangalan = prompt("Ano pangalan mo?")
+// alert("Halu " + pangalan);
+
